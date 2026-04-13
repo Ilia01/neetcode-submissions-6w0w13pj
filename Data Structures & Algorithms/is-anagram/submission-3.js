19ff852b511c2fec1075021c25884405212981f1
Ilia <155351862+Ilia01@users.noreply.github.com>
2026-04-13 14:02:34 +0400

    Add: is-anagram - submission-3

---
+++ Data Structures & Algorithms/is-anagram/submission-3.js
@@ -0,0 +1,23 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {string} t
+     * @return {boolean}
+     */
+    isAnagram(s, t) {
+        if (s.length !== t.length) return false;
+
+        const hash = {};
+
+        for (let i = 0; i < s.length; i++) {
+            hash[s[i]] = (hash[s[i]] || 0) + 1;
+            hash[t[i]] = (hash[t[i]] || 0) - 1;
+        }
+
+        for (const key in hash) {
+            if (hash[key] !== 0) return false;
+        }
+
+        return true;
+    }
+}
