8eca298de0b7de0d980c201c0bb6cfad3c80bc00
Ilia <155351862+Ilia01@users.noreply.github.com>
2026-06-05 11:57:25 +0400

    Add: two-integer-sum - submission-5

---
+++ Data Structures & Algorithms/two-integer-sum/submission-5.ts
@@ -0,0 +1,24 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {number[]}
+     */
+    twoSum(nums: number[], target: number): number[] {
+        const map = {};
+
+        for (let i = 0; i < nums.length; i++) {
+            let num = nums[i];
+            let sub = target - num;
+
+            if (isFinite(map[sub])) {
+                return [map[sub], i]
+            }
+            if (!map[num]) {
+                map[num] = i;
+            }
+        };
+
+        return []
+    }
+}
